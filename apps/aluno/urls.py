846bec712a438eb59c519bb621e858ac9f15895f
Alexandre Rocha <alexandresobral2004@gmail.com>
2021-09-30 20:30:31 -0300

    'edit_delete_comit'

--- apps/aluno/urls.py
+++ apps/aluno/urls.py
@@ -1,3 +1,22 @@
+# from django.contrib import admin
+# from django.urls import include, path
+# from apps.core import views
+
+
+# app_name = 'aluno'
+
+# from . import views
+
+# urlpatterns = [
+
+#     #adicionando a view do core
+
+#     path('',views.AddAluno,name='add_aluno'),
+#     path('lista/',views.lista_Alunos,name='lista_alunos'),
+   
+  
+# ]
+
 from django.contrib import admin
 from django.urls import include, path
 from apps.core import views
@@ -10,8 +29,13 @@ from . import views
 urlpatterns = [
 
     #adicionando a view do core
-
-    path('',views.AddAluno,name='add_aluno'),
-    path('alunos/lista',views.lista_alunos,name='lista_alunos'),
-
+    path('',views.HomeView.as_view(),name='home'),
+    path('novo/',views.AddAluno,name='add_aluno'),
+    path('lista/',views.ListAlunosView.as_view(),name='lista_alunos'),
+    path('editar/<int:id_aluno>/',views.edit_aluno,name='edit_aluno'),
+    path('delete/<int:id_aluno>/',views.deleteAluno,name='delete_aluno'),
+   
+   
+  
 ]
+
--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -2,9 +2,67 @@ from django.shortcuts import render,redirect, get_list_or_404,get_object_or_404
 from aluno.models  import Aluno
 from django.contrib import messages
 from aluno.forms import AlunoForm
+from django.views.generic import TemplateView,CreateView,ListView
 
+
+
+# def novoAluno(request):
+#    template_name = 'aluno/add_aluno.html'
+#    context = {}
+#    form = AlunoForm()
+#    context['form'] = form
+#    return render(request,template_name,context)
+
+class HomeView(TemplateView):
+   template_name = 'aluno/lista_alunos.html'
+   
+
+# class AddAlunoView(CreateView):
+#       template_name = 'aluno/add_aluno.html'
+#       model = Aluno
+#       form_class = AlunoForm
+#       messages.success( "Usuário salvo com sucesso!")
+#       success_url = 'aluno/add_aluno.html' 
+    
+
+
+
+class ListAlunosView(ListView):
+   model = Aluno
+   template_name = 'aluno/lista_alunos.html'
+   context_object_name = 'alunos'
+   paginate_by = 6
+
+   def get_queryset(self):
+       return Aluno.objects.all().order_by('-id')
+
+
+def deleteAluno(request, id_aluno):
+    aluno = Aluno.objects.get(id=id_aluno)
+    aluno.delete()
+    messages.success(request, "Aluno deletado com sucesso!")
+    return redirect('aluno:lista_alunos')
+
+
+def edit_aluno(request, id_aluno):
+    template_name = 'aluno/add_aluno.html'
+    context = {}
+    aluno = get_object_or_404(Aluno, pk=id_aluno)
+    if request.method == 'POST':
+        form = AlunoForm(request.POST, instance=aluno)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Aluno editado com sucesso!")
+            return redirect('aluno:lista_alunos')
+    form = AlunoForm(instance=aluno)
+    context['form'] = form
+    return render(request, template_name, context)
 
 # Create your views here.
+
+
+# def home(request):
+#     return render(request,'base.html',{})
 
 
 
@@ -14,7 +72,7 @@ def AddAluno(request):
     if request.method == 'POST':
         form = AlunoForm(request.POST)
         if form.is_valid():
-            # f = form.save(commit=False)
+            f = form.save(commit=False)
             form.save()
             messages.success(request, "Usuário salvo com sucesso!")
         else:
@@ -25,10 +83,46 @@ def AddAluno(request):
 
 
 
-def lista_alunos(request):
-    template_name = 'aluno/lista_alunos.html'
-    alunos = Aluno.objects.all().reverse()
-    context = {
-       'alunos':alunos 
-    }
-    return render(request,template_name,context)
+# def lista_Alunos(request):
+#     template_name = 'aluno/lista_alunos.html'
+#     alunos = Aluno.objects.all().reverse()
+#     context = { 'alunos':alunos  }
+#     print(context)
+#     return render(request,template_name,context)
+
+
+
+
+# from django.shortcuts import render,redirect, get_list_or_404,get_object_or_404
+# from aluno.models  import Aluno
+# from django.contrib import messages
+# from aluno.forms import AlunoForm
+
+
+# # Create your views here.
+
+
+
+# def AddAluno(request):
+#     template_name = 'aluno/add_aluno.html'
+#     context = {}
+#     if request.method == 'POST':
+#         form = AlunoForm(request.POST)
+#         if form.is_valid():
+#             # f = form.save(commit=False)
+#             form.save()
+#             messages.success(request, "Usuário salvo com sucesso!")
+#         else:
+#             messages.error(request, "Erro ao salvar dados!")
+#     form = AlunoForm()
+#     context['form'] = form
+#     return render(request,template_name,context)
+
+
+
+# def lista_Alunos(request):
+#     template_name = 'aluno/lista_alunos.html'
+#     alunos = Aluno.objects.all().reverse()
+#     context = { 'alunos':alunos  }
+#     print(context)
+#     return render(request,template_name,context)
